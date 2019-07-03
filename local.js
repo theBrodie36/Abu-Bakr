c2fc7a5340c8eaa82704a3420497ecfa5d2a5e08
theBrodie36 <36389004+theBrodie36@users.noreply.github.com>
2019-07-02 22:20:03 -0700

    Add files via upload

---
+++ local.js
@@ -0,0 +1,9 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
+window.addEventListener('scroll', function() {
+  console.log("Scrollin'");
+});
